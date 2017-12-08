b31415a81753b7aefcca0189449d8523959fc91b
KidPluto <tigers-02121@pobox.com>
2017-12-07 21:55:09 -0500

    Initial commit

---
+++ src/main/java/net/kidpluto/RESTfulCalendarExample/ResTfulCalendarExampleApplication.java
@@ -0,0 +1,12 @@
+package net.kidpluto.RESTfulCalendarExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulCalendarExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulCalendarExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/net/kidpluto/RESTfulCalendarExample/ResTfulCalendarExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package net.kidpluto.RESTfulCalendarExample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ResTfulCalendarExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
