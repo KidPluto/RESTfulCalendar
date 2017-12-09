1c9a28cc23a5d6ed2faed8cbe8bb1121522894e2
KidPluto <tigers-02121@pobox.com>
2017-12-09 17:57:13 -0500

    Sanity ci

--- src/main/java/net/kidpluto/RESTfulCalendarExample/Calendar.java
+++ src/main/java/net/kidpluto/RESTfulCalendarExample/Calendar.java
@@ -1,11 +1,8 @@
 package net.kidpluto.RESTfulCalendarExample;
 
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Context;
-import java.io.IOException;
 import java.util.Date;
 import java.util.Set;
 
@@ -35,30 +32,30 @@ public interface Calendar {
     @Path("/create/meeting")
     @Produces(MediaType.APPLICATION_XML)
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    String createMeeting(@FormParam("calenderId") int calendarId,
-                         @FormParam("start") Date start,
-                         @FormParam("finish") Date finish,
-                         @FormParam("description") String description,
-                         @FormParam("attendees") Set attendees,
-                         @Context HttpServletResponse servletResponse) throws IOException;
+    Response createMeeting(
+            @FormParam("calenderId") int calendarId,
+            @FormParam("start") Date start,
+            @FormParam("finish") Date finish,
+            @FormParam("description") String description,
+            @FormParam("attendees") Set attendees);
 
     @PUT
     @Path("/update/meeting")
     @Produces(MediaType.APPLICATION_XML)
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    String updateMeeting(@FormParam("calenderId") int calendarId,
-                         @FormParam("meetingId") int meetingId,
-                         @FormParam("start") Date start,
-                         @FormParam("finish") Date finish,
-                         @FormParam("description") String description,
-                         @FormParam("attendees") Set attendees,
-                         @Context HttpServletResponse servletResponse) throws IOException;
+    Response updateMeeting(
+            @FormParam("calenderId") int calendarId,
+            @FormParam("meetingId") int meetingId,
+            @FormParam("start") Date start,
+            @FormParam("finish") Date finish,
+            @FormParam("description") String description,
+            @FormParam("attendees") Set attendees);
 
     @PUT
     @Path("/sync/calendars")
     @Produces(MediaType.APPLICATION_XML)
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    String syncCalendars(@FormParam("calenderOneId") int calendarOneId,
-                         @FormParam("calendarTwoId") int calendarTwoId,
-                         @Context HttpServletResponse servletResponse) throws IOException;
+    Response syncCalendars(
+            @FormParam("calenderOneId") int calendarOneId,
+            @FormParam("calendarTwoId") int calendarTwoId);
 }
